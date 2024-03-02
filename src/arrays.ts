b3f6f0ee4a76a60a52d59f963c81da0ad55ebafd
thernd302 <thernd@udel.edu>
2024-03-02 14:15:33 -0500

    task5

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,15 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length === 0) {
+        return [];
+    }
+
+    if (numbers.length === 1) {
+        return [numbers[0], numbers[0]];
+    }
+
+    return [numbers[0], numbers[numbers.length - 1]];
 }
 
 /**
@@ -13,7 +21,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripledNumbers = numbers.map((number) => number * 3);
+    return tripledNumbers;
 }
 
 /**
@@ -21,7 +30,11 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const stringsToIntegers = numbers.map((string) => {
+        const parsedValue = parseInt(string, 10);
+        return isNaN(parsedValue) ? 0 : parsedValue;
+    });
+    return stringsToIntegers;
 }
 
 /**
@@ -32,7 +45,14 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    return amounts.map((string) => {
+        const removeDollars = parseInt(string.replace(/^\$/, ""), 10);
+        if (isNaN(removeDollars)) {
+            return 0;
+        } else {
+            return removeDollars;
+        }
+    });
 };
 
 /**
@@ -41,7 +61,11 @@ export const removeDollars = (amounts: string[]): number[] => {
  * in question marks ("?").
  */
 export const shoutIfExclaiming = (messages: string[]): string[] => {
-    return [];
+    return messages
+        .filter((message) => !message.endsWith("?")) // Remove strings ending in "?"
+        .map((message) =>
+            message.endsWith("!") ? message.toUpperCase() : message
+        );
 };
 
 /**
@@ -49,7 +73,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const countShortWords = words.filter((words) => words.length < 4);
+    return countShortWords.length;
 }
 
 /**
@@ -58,7 +83,12 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    if (colors.length === 0) {
+        return true;
+    }
+    const allRGB = (colors: string) =>
+        ["red", "blue", "green"].includes(colors);
+    return colors.every(allRGB);
 }
 
 /**
@@ -69,9 +99,15 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
-}
+    const sum = addends.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+    );
+    const numbersString = addends.join("+");
+    const resultString = `${sum}=${numbersString || 0}`;
 
+    return resultString;
+}
 /**
  * Consumes an array of numbers and produces a new array of the same numbers,
  * with one difference. After the FIRST negative number, insert the sum of all
@@ -82,5 +118,24 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    return [];
+    let sum = 0;
+    let foundNegative = false;
+    const result = [];
+
+    for (const num of values) {
+        if (num < 0 && !foundNegative) {
+            foundNegative = true;
+            result.push(num);
+            result.push(sum);
+        } else {
+            sum += num;
+            result.push(num);
+        }
+    }
+
+    if (!foundNegative) {
+        result.push(sum);
+    }
+
+    return result;
 }
